84807b4dddf092bf6999c5d7dc9ae6e1e022065f
Muskan <muskanyav06132013@gmail.com>
2026-01-18 23:28:40 +0530

    Add files via upload

---
+++ EXPERIMENT1/Code/logs.js
@@ -0,0 +1,13 @@
+export const logs = [
+  { id: 1, activity: "Car Travel", carbon: 4 },
+  { id: 2, activity: "Electricity Usage", carbon: 6 },
+  { id: 3, activity: "Cycling", carbon: 0 },
+  { id: 3, activity: "Riding a Bike", carbon: 0 },
+  { id: 4, activity: "Taking the Bus", carbon: 2 },
+  { id: 5, activity: "Electric Car Travel", carbon: 2 },
+  { id: 6, activity: "Using Solar Panels", carbon: 0 },
+  { id: 7, activity: "Air Travel", carbon: 10 },
+  { id: 8, activity: "Cooking with Electric Stove", carbon: 2 },
+  { id: 9, activity: "Planting Trees", carbon: -1 },
+  { id: 10, activity: "Recycling Plastic", carbon: 0 },
+];
